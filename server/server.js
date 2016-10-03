46b9416629c1292eff1f4fa946eb7ae816feee62
Chris Kuh <kuh_chris@yahoo.com>
2016-10-03 12:11:51 -0700

    [feature] setup client

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const RRM = require('./helpers/RRM.js')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParse = require('body-parser')
+
+const app = express()
+app.set('port', process.env.PORT || 1337)
+
+dotenv.load()
+app.use(cors())
+app.use(bodyParse.json())
+
+app.use('/public', express.static('./public'))
+
+app.use(RRM)
+
+console.log('Listening on port ', app.get('port'))
+app.listen(app.get('port'))
