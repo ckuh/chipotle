fc43fb65048de968b33ed088e2702f570051611e
Chris Kuh <kuh_chris@yahoo.com>
2016-10-05 19:25:19 -0700

    [feature] setup ingredients server/client

--- js/Home.jsx
+++ js/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = (props) => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-)
+class Home extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      data: []
+    }
+
+    this.renderData = this.renderData.bind(this)
+  }
+
+  componentDidMount () {
+    this.getData()
+  }
+
+  getData () {
+    return axios.get(
+      '/api/ingredients'
+    )
+    .then((resp) => {
+      this.setState({data: resp.data})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  renderData () {
+    return this.state.data.map((item, key) => {
+      return (
+        <div key={key}>
+          <h1>{item.name}</h1>
+          <img src={item.url} />
+        </div>
+      )
+    })
+  }
+
+  render () {
+    let data
+
+    if (this.state.data) {
+      data = this.renderData()
+    }
+
+    return (
+      <div>
+        <h1>Hello World</h1>
+        {data}
+      </div>
+    )
+  }
+}
 
 export default Home
